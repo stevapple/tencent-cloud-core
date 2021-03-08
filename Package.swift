fa19aaadc4dd28a0eed414a8f78cf4e42e152335
stevapple <stevapple@icloud.com>
2021-03-08 23:46:07 +0800

    Deprecate TencentCloudAPI

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
     products: [
         .library(name: "TencentCloudCore", targets: ["TencentCloudCore"]),
         .library(name: "TencentCloudAPICore", targets: ["TencentCloudAPICore"]),
-        .library(name: "TencentCloudAPI", targets: ["TencentCloudAPI"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto", from: "1.0.0"),
@@ -20,7 +19,5 @@ let package = Package(
         .testTarget(name: "TencentCloudCoreTests", dependencies: ["TencentCloudCore"]),
         .target(name: "TencentCloudAPICore", dependencies: ["TencentCloudCore", "Crypto"]),
         .testTarget(name: "TencentCloudAPICoreTests", dependencies: ["TencentCloudAPICore"]),
-        .target(name: "TencentCloudAPI", dependencies: ["TencentCloudAPICore"]),
-//        .testTarget(name: "TencentCloudAPITests", dependencies: ["TencentCloudAPI"]),
     ]
 )
